e63c553600bfefe0ee985a139221d01d4f5ea3c7
Josh Preston <jdp.github@prestoncentral.com>
2015-03-07 21:14:33 -0500

    adding notifies for debugging

--- manifests/host.pp
+++ manifests/host.pp
@@ -58,6 +58,7 @@ define dnsmasq::host (
       default => $ensure,
     }
     if str2bool($::dnsmasq::exported) {
+      notify { 'attempting to use exported resources for ethers': }
       @@common::line { "dnsmasq::ethers ${h_real} ${mac_r}":
         ensure => $ethers_ensure,
         file   => $dnsmasq::ethers_file,
@@ -86,6 +87,7 @@ define dnsmasq::host (
     default => $ensure,
   }
   if str2bool($::dnsmasq::exported) {
+    notify { 'attempting to use exported resources for hosts': }
     @@common::line { "dnsmasq::hosts ${h_real} ${ip}":
       ensure => $hosts_ensure,
       file   => $::dnsmasq::hosts_file,
